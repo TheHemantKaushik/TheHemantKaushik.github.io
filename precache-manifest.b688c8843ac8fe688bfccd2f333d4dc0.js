4e9335011b7b13a09f7220fa9b9e8ef1c38f8406
Hemant Kaushik <thehemantkaushik@gmail.com>
2019-02-02 03:20:25 +0530

    Updates in designs, work view removed, about and contact merged.

---
+++ precache-manifest.b688c8843ac8fe688bfccd2f333d4dc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65a1fb5df71d884f71dbb828e2eaa71b",
+    "url": "/static/media/about-pic.65a1fb5d.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "454c0b2e8f86cfa44eda",
+    "url": "/static/js/main.454c0b2e.chunk.js"
+  },
+  {
+    "revision": "1d189b5450da07a73245",
+    "url": "/static/js/1.1d189b54.chunk.js"
+  },
+  {
+    "revision": "454c0b2e8f86cfa44eda",
+    "url": "/static/css/main.75a31974.chunk.css"
+  },
+  {
+    "revision": "5e029708d48759581a1d4889bf74b026",
+    "url": "/index.html"
+  }
+];
